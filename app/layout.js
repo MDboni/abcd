f124e93154396bd1c416452fad6deaea54f3f926
MD BONI AMIN JAYED <106344301+MDboni@users.noreply.github.com>
2026-02-21 22:19:07 +0600

    Revert "Refactor layout and simplify homepage structure"

--- app/layout.js
+++ app/layout.js
@@ -22,9 +22,6 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ul>
-          <li>about</li>
-        </ul>
         {children}
       </body>
     </html>
